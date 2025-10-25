bbf540e6bd194aacfbc934bd8bedfa165857f1a6
Augusto Reyes <jreyess16@miumg.edu.gt>
2025-10-24 23:46:35 -0600

    Descripción de los cambios que hiciste

--- api/record_payment.php
+++ api/record_payment.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: https://pos-c7b3etcee5bnczbm.westus-01.azurewebsites.net");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Responder a preflight OPTIONS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 session_start();
 header('Content-Type: application/json; charset=utf-8');
 
